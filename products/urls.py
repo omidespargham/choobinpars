6db4c25bf848a2bf2d8c59fe7bc86d32705b5121
amir <amirarsalannazanin6@gmail.com>
2022-12-16 17:39:20 +0330

    add product added with form and other things

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('detail/<int:id>/', views.ProductDetailView.as_view(),
          name='product_detail'),
     # path("categorys/",views.Categorys.as_view(),name="category"),
-    path("<slug:category_slug>/",views.ProductsWithSpecificCategoryView.as_view(),name="show_products"),
-    
+    path('add_product/', views.AddProductView.as_view(), name='add_product'),
+    path("<slug:category_slug>/", views.ProductsWithSpecificCategoryView.as_view(), name="show_products"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import View
-from .models import Product,Category
+
+from products.forms import AddProductForm
+from .models import Product, Category
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ProductDetailView(View):
@@ -14,6 +18,41 @@ class ProductDetailView(View):
             return redirect("home:home")
 
 
+class AddProductView(View, LoginRequiredMixin):
+    form_class = AddProductForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.get_username() == '':
+            return redirect('home:home')
+        else:
+            if request.user.is_admin == True:
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                return redirect('home:home')
+
+    def get(self, request):
+        return render(request, 'products/addproduct.html', {'form': self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cl = form.cleaned_data
+            category = Category.objects.get(name=cl['category'])
+            product = Product(
+                user=request.user,
+                # category=category,
+                name=cl['name'],
+                price=cl['price'],
+                slug=cl['slug'],
+                description=cl['description'],
+            )
+            product.save()
+            product.category.add(category)
+            messages.success(request, f"add product", "success")
+            return redirect('home:home')
+        return render(request, 'products/addproduct.html', {'form': form})
+
+
 # class Categorys(View):
 #     def get(self, request):
 #         categorys = Category.objects.filter(parent__isnull=True)
@@ -23,17 +62,13 @@ class ProductDetailView(View):
 #         pass
 
 
-class ProductsWithSpecificCategoryView(View): # this is for search
-    def get(self,request,category_slug):
+class ProductsWithSpecificCategoryView(View):  # this is for search
+    def get(self, request, category_slug):
         try:
             category = Category.objects.get(slug=category_slug)
             products = Product.objects.filter(category=category)
         except Category.DoesNotExist:
             products = None
-        return render(request,"products/products.html",{"products":products})
-
-
-
-
+        return render(request, "products/all_products.html", {"products": products})
 
 # Create your views here.
